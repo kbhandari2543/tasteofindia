c0554bc98e962135dcc06671a4ab48795fd78a4f
bobinbc <bobinbc@gmail.com>
2014-12-11 20:25:24 -0500

    updated index.js to remove warnings.

--- js/index.js
+++ js/index.js
@@ -1,30 +1,42 @@
-$(document).ready(function(){
+/*global $, jQuery, alert*/
 
-function initGeolocation() {
-        navigator.geolocation.getCurrentPosition(getLocation, errorHandling);
-    }
-    function errorHandling(error) {
-        switch(error.code) {
-            case error.PERMISSION_DENIED: alert("You are not sharing your location.");
-            break;
-            case error.POSITION_UNAVAILABLE: alert("Could not detect your position.");
-            break;
-            case error.TIMEOUT: alert("Position retrieval timed out.");
-            break;
-            default: alert("Unknown error.");
-            break;
-        }
-    }
+$(document).ready(function () {
+    "use strict";
+
     function getLocation(position) {
         var url = "http://maps.google.com/maps/api/staticmap?sensor=false&center=" +
             position.coords.latitude + "," +
             position.coords.longitude + "&zoom=14&size=200x300&markers=color:red|" +
             position.coords.latitude + ',' +
             position.coords.longitude;
-        $("#map").attr("src", url)
+        $("#map").attr("src", url);
     }
-   initGeolocation();
+
+    function errorHandling(error) {
+        switch (error.code) {
+        case error.PERMISSION_DENIED:
+            alert("You are not sharing your location.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Could not detect your position.");
+            break;
+        case error.TIMEOUT:
+            alert("Position retrieval timed out.");
+            break;
+        default:
+            alert("Unknown error.");
+            break;
+        }
+    }
+
+    function initGeolocation() {
+        navigator.geolocation.getCurrentPosition(getLocation, errorHandling);
+    }
+
+
+
+
+    initGeolocation();
 
 
 });
-
